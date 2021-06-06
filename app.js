ced808eaa61fb02596560c8947166b3c81065363
area268457913 <area268457913@hotmail.com>
2021-06-06 20:07:42 +0800

    feat: add new page

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const db = mongoose.connection
 const exphbs = require('express-handlebars')
 const Cate = require('./models/category')
 const Rec = require('./models/record')
+const bodyParser = require('body-parser')
 
 
 app.use(express.static('public'))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -38,6 +41,31 @@ app.get('/', (req, res) => {
       res.render('index', { recs, totalAmount, cates })
     })
     .catch(error => console.error(error))
+})
+
+app.get('/recs/new', (req, res) => {
+  const cates = []
+  Cate.find()
+    .lean()
+    .then((item) => {
+      item.forEach((items) => cates.push(items.name))
+    })
+  return res.render('new', { cates })
+})
+
+app.post('/recs', (req, res) => {
+  const record = req.body
+  const category = req.body.category
+
+  Cate.findOne({ name: category })
+    .lean()
+    .then(function (item) {
+      return (record.icon = item.icon)
+    })
+    .then(() => {
+      Rec.create(record).then(() => res.redirect('/'))
+    })
+    .catch((error) => console.log(error))
 })
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
